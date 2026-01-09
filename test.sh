a3a30cda4f7d1dbe5ee6aeed2668f5b72d3c37f5
Meme Tech <98437347+Pokebrouserkat@users.noreply.github.com>
2026-01-09 10:01:20 -0500

    Add test.sh script for running GdUnit4 tests

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Run GdUnit4 tests
+
+GODOT="/Users/matt/Library/Application Support/Steam/steamapps/common/Godot Engine/Godot.app/Contents/MacOS/Godot"
+"$GODOT" --headless -s addons/gdUnit4/bin/GdUnitCmdTool.gd -a test/ --ignoreHeadlessMode "$@"
